dcd2d4920446db14ce6f9d7c7379d4ee96abc598
Quazi Ghulam Rafi <quazirafi1@gmail.com>
2025-04-21 14:11:33 +0100

    azure authenticated and vms created with terraform

---
+++ main.tf
@@ -0,0 +1,16 @@
+# good practice but not required, enables you to manage the changes that comes with version release
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {
+
+  }
+}
